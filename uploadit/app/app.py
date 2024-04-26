293691a93eb658ca6e91cd9e89c377f093d8cdc3
s4:D_t0y <roccowu949@gmail.com>
2024-04-26 10:31:28 -0700

    Remove Desktop Application Files

--- uploadit/app/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("UploadIt")
