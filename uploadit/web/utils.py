8417f403819974add6a3703b292405bef904eb95
s4:D_t0y <roccowu949@gmail.com>
2024-04-26 10:13:06 -0700

    Upload Works

--- uploadit/web/utils.py
+++ uploadit/web/utils.py
@@ -4,8 +4,7 @@ import json
 
 
 def random_string():
-    return "".join(random.choice(string.ascii_letters) for i in range(10))
-
+    return "".join(random.choice(string.ascii_letters) for i in range(50))
 
 def get_config():
     with open("files.json") as file_json:
--- uploadit/web/web.py
+++ uploadit/web/web.py
@@ -8,11 +8,11 @@ from flask import (
     url_for,
     render_template,
     request,
-    jsonify,
+    current_app,
 )
 from werkzeug.utils import secure_filename
 from pathlib import Path
-import secrets
+import json
 import os
 
 home_page = Blueprint("index", __name__)
@@ -20,7 +20,8 @@ download_page = Blueprint("download", __name__)
 upload_page = Blueprint("upload", __name__)
 favicon_page = Blueprint("favicon", __name__)
 
-files = get_config()
+json_db = get_config()
+print(json_db)
 
 @home_page.route("/", methods=["GET"])
 def index():
@@ -32,12 +33,12 @@ def download():
         data = request.form
         requested_id = data.get("download_id")
         if requested_id != None:
-            print("is none")
-            valid_id_list = files.keys()
-
+            valid_id_list = json_db.keys()
+            print(valid_id_list)
             if requested_id in valid_id_list:
-                print("yess")
-                return send_file(files[requested_id], as_attachment=True)
+                upload_dir = current_app.config["UPLOAD_DIRECTORY"]
+                return_file = f'{upload_dir}{json_db[requested_id]}'
+                return send_file(return_file, as_attachment=True)
             else:
                 return abort(403)
         else:
@@ -45,26 +46,21 @@ def download():
     elif request.method == "GET":
         return render_template("download.html")
 
-@upload_page.route('/upload', methods=['POST', 'GET'])
+@upload_page.route('/', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         try:
             file = request.files["file"]
-            print(file)
         except KeyError:
             return "No file chosen", 422
-        print(request.form)
+
         file_uuid = request.form["dzuuid"]
         # Generate a unique filename to avoid overwriting using 8 chars of uuid before filename.
         filename = f"{file_uuid[:8]}_{secure_filename(file.filename)}"
-        save_path = Path("uploads", filename)
+        save_path = Path(current_app.config["UPLOAD_DIRECTORY"], filename)
+        print(save_path)
         current_chunk = int(request.form["dzchunkindex"])
 
-        if not os.path.exists(save_path):
-
-            print(os.listdir())
-            f = open(filename, "w")
-            f.close()
         try:
             with open(save_path, "ab") as f:
                 f.seek(int(request.form["dzchunkbyteoffset"]))
@@ -79,9 +75,17 @@ def upload():
             # This was the last chunk, the file should be complete and the size we expect
             if os.path.getsize(save_path) != int(request.form["dztotalfilesize"]):
                 return "Size mismatch.", 500
+            
+        with open('files.json', 'r') as f:
+            json_data = json.load(f)
 
-        return "Chunk upload successful.", 200
+        data = {f'{random_string()}': f'{filename}'}
+        json_data.update(data)
 
+        with open('files.json', 'w') as f:
+            json.dump(json_data, f)
+        return "Upload Completed", 200
+    
     elif request.method == 'GET':
         return render_template('upload.html')
 
@@ -92,9 +96,11 @@ def favicon():
 def serve():
     app = Flask(__name__)
 
+    app.config["UPLOAD_DIRECTORY"] = app.root_path + "/uploads/"
+
     app.register_blueprint(home_page)
     app.register_blueprint(download_page)
-    app.register_blueprint(upload_page)
+    app.register_blueprint(upload_page, url_prefix="/upload")
     app.register_blueprint(favicon_page)
 
     return app.run(host="0.0.0.0", port="1212", debug=True)
